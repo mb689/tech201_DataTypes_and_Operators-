6029bf6a289f833350b45b362c5ee4d597f1b437
Mohamed Boudjatat <mboudjatat17@hotmail.com>
2023-01-30 14:12:31 +0000

    Boolean and noneType

---
+++ boolean_and_none.py
@@ -0,0 +1,28 @@
+# Booleans
+
+# True or False
+
+# a = True
+# b = False
+#
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+#
+
+hi = "hello world!"
+
+print(hi.isalpha())
+print(hi.islower())
+print(hi.isupper())
+print(hi.endswith("orld!"))
+print(hi.startswith("h"))
+
+x = 0
+y = 1
+
+print(bool(x))
+print(bool(y))
+
+
+print(bool(None))
